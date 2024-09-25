3cff37e17a6d0a2a5d4e988638e628d631286592
KshithijKeshav <145021571+KshithijKeshav@users.noreply.github.com>
2024-09-26 01:38:59 +0530

    Fixed initialization

--- init_project.py
+++ init_project.py
@@ -4,7 +4,7 @@ import subprocess
 # Create virtual environment and install Python dependencies
 def setup_backend():
     print("Setting up Python virtual environment...")
-    subprocess.run(['python3', '-m', 'venv', 'backend'])
+    subprocess.run(['python3', '-m', 'venv', 'Backend/backend'])
     subprocess.run(['Backend/backend/bin/pip', 'install', '-r', 'Backend/requirements.txt'])
     print("Backend setup complete!")
 
